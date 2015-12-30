88220faa952c2316554cfa35c61a72f9757cc610
amsakib <sakib.rangpur@gmail.com>
2015-12-31 01:38:57 +0600

    implemented some OOP features

---
+++ includes/database.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asir Mosaddek Sakib
+ * Date: 12/30/2015
+ * Time: 11:01 PM
+ */
+
+class Database {
+
+}
---
+++ includes/user.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asir Mosaddek Sakib
+ * Date: 12/30/2015
+ * Time: 10:57 PM
+ */
+
+class User {
+
+}
